49d115092107d9b2417d99e69d431a00ef9bff58
SH <zaeni@Zaenis-MacBook-Pro.local>
2024-01-26 19:10:35 +0700

    crud users

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,69 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
 import 'package:todo_app/home.dart';
 import 'package:flutter_native_splash/flutter_native_splash.dart';
+import 'package:todo_app/model/user.dart';
 import 'package:todo_app/screens/login_screen.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:http/http.dart';
 
 final Color BLACK_CUSTOM = const Color(0xFF000000);
 final Color GREEN_PRIMARY = const Color(0xFF4EA949);
 final Color YELLOW_CUSTOM = const Color(0xFFFFC83F);
 
-void main() {
+void addUser() async {
+  final FirebaseFirestore db = FirebaseFirestore.instance;
+  final CollectionReference users = db.collection("users");
+  final Map<String, dynamic> userValue = {
+    "user_id": 3,
+    "user_name": "Dena",
+    "user_email": "dena@gmail.com"
+  };
+  await users.doc("dena@gmail.com").set(userValue);
+}
+
+void updateUser() async {
+  final FirebaseFirestore db = FirebaseFirestore.instance;
+  final CollectionReference users = db.collection("users");
+  final Map<String, dynamic> userValue = {
+    "user_name": "Dena Baru",
+  };
+  await users.doc("dena@gmail.com").update(userValue);
+}
+
+void deleteUser() async {
+  final FirebaseFirestore db = FirebaseFirestore.instance;
+  final CollectionReference users = db.collection("users");
+  await users.doc("dena@gmail.com").delete();
+}
+
+void main() async {
   WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+      options: const FirebaseOptions(
+          apiKey: "AIzaSyBkluq8V_hzI3SNOUl92ty56WLfmp3wFDg",
+          appId: "1:395030047429:android:6f972f8d07833f92ba5eac",
+          messagingSenderId: "395030047429",
+          projectId: "todo-list-4d4a1"));
+
+  final FirebaseFirestore db = FirebaseFirestore.instance;
+  final CollectionReference users = db.collection("users");
+  // addUser();
+  // updateUser();
+  deleteUser();
+  users.get().then(
+    (querySnapshot) {
+      print("Successfully completed");
+      for (var docSnapshot in querySnapshot.docs) {
+        print('${docSnapshot.id} => ${docSnapshot.data()}');
+      }
+    },
+    onError: (e) => print("Error completing: $e"),
+  );
+
+  
   FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
   runApp(MyApp());
   FlutterNativeSplash.remove();
@@ -19,7 +74,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: HomeScreen(),
+      home: MyHomePage(),
       theme: ThemeData(fontFamily: 'Poppins'),
     );
   }
