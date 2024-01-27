ee63a05420a7ff1c164c523dc248f31711734023
Fikri Muhamad Fahmi <fikri.muhamad2013@gmail.com>
2024-01-27 18:22:12 +0700

    hapus class MyHome

--- lib/main.dart
+++ lib/main.dart
@@ -130,7 +130,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: IntroPage(),
+      home: const IntroPage(),
       theme: ThemeData(fontFamily: 'Poppins'),
     );
   }
@@ -172,80 +172,6 @@ class _IntroPageState extends State<IntroPage> {
     super.dispose();
   }
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            RichText(
-              text: TextSpan(
-                children: [
-                  TextSpan(
-                    text: 'MANAGE ',
-                    style: TextStyle(
-                      fontSize: 24.0,
-                      fontWeight: FontWeight.bold,
-                      color: Color(0xFF001A72),
-                    ),
-                  ),
-                  TextSpan(
-                    text: 'YOUR TASKS',
-                    style: TextStyle(
-                      fontSize: 24.0,
-                      fontWeight: FontWeight.bold,
-                      color: Color(0xFFE3A60D),
-                      height: 1.0,
-                    ),
-                  ),
-                ],
-              ),
-            ),
-            SizedBox(height: 0.0),
-            Text(
-              'EASILY',
-              style: TextStyle(
-                fontSize: 80.0,
-                fontWeight: FontWeight.bold,
-                color: GREEN_PRIMARY,
-                height: 1.0,
-              ),
-            ),
-            SizedBox(height: 20.0),
-            Image.asset(
-              'assets/images/hero.png',
-              width: 300.0,
-              height: 300.0,
-            ),
-            SizedBox(height: 20.0),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => LoginScreen(),
-                  ),
-                );
-              },
-              style: ElevatedButton.styleFrom(
-                backgroundColor: GREEN_PRIMARY,
-                foregroundColor: Colors.white,
-                padding: const EdgeInsets.symmetric(
-                  vertical: 15.0,
-                  horizontal: 20.0,
-                ),
-              ),
-              child: Text('Get Started'),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-}
-
-class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
